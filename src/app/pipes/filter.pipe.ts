b88df05e93026babf700d49aad247a82d63ca61c
Ben-sz <sz.bence53@gmail.com>
2020-08-27 22:55:32 +0200

    default filter now shows all pokemon

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -16,11 +16,13 @@ export class FilterPipe implements PipeTransform {
     };
 
 
-
     /* Filters list of pokemons depending on their type*/
     /* If pokemon has 2 types check if any is equal to type*/
     /* If pokemon has 1 types check if it is equal to type*/
     /* else false */
+    if (type == undefined){
+      return values;
+    }
 
     values = values.filter(function (element) {
         if  (element.types[1] !== undefined && (element.types[0].type.name == type || element.types[1].type.name == type)){
